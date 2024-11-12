5a13c86aef0bae20867ff30947d5b92751bf8023
Tom Gemi <gemitomi@hotmail.com>
2024-11-13 00:06:33 +0100

    session _1

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-const app = express()
+import session from 'express-session';
+const app = express();
 const port = 3000
+
+app.use('/static', express.static('public'));
+
+app.use(session({
+  secret: 'kjk56jk65jknkj5k',
+  resave: false,
+  saveUninitialized: true
+}))
+
+function authMW(req, res, next){
+  if (req.session.belepve !== 12) {
+    console.log("Nix belépve!");
+    return res.redirect('/');
+  }
+  console.log("Minden fasza!");
+  return next();
+}
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -19,26 +37,52 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
-app.use('/myuploads', express.static('uploads'));
+app.use('/myuploads', authMW, express.static('uploads'));
 
-app.use('/static', express.static('public'));
+app.get('/logout', authMW, (req, res, next) => {
+  return req.session.destroy(err => {
+    console.log(err);
+    return res.redirect('/');
+  })
+})
 
-app.post('/', upload.single ("alma"), (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
+app.post('/', authMW, upload.single ("alma"), (req, res, next) => {
+  //console.log(req.file);
+  console.log(req.body.text);
   res.send('Hello World!');
 })
 
 app.get('/', (req, res, next) => {
+  console.log(req.session);
   fs.readdir('./uploads', (err, fileList) => {
-    let html = "<html><body><a href='/login.html'/>";
+    let html = "<html><body>";
+    if (req.session.belepve === 12) {
+      html += "<a href='/logout'/>Logout</a><br/>";
+    } else {
+      html += "<a href='/static/login.html'/>Login</a><br/>";
+    }
     for (let i=0; i<fileList.length; i++) {
       html += `<img src='myuploads/${fileList[i]}' style="padding:30px">`
     }
     return res.send(html);
   });
 });
-//454
+
+app.post('/login', upload.none(), (req, res, next) => {
+  if (typeof req.body.jelszo === 'undefined'){
+    return res.status(404).end("no");
+  }
+
+  if (req.body.jelszo === "asdasd") {
+    req.session.belepve = 12;
+    return req.session.save((err) => {
+      console.log(err);
+      return res.redirect('/');
+    }) 
+  }
+
+  return res.redirect('/static/login.html');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
